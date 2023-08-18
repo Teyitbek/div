6e907997e00fd553786dcf7e7f96608329154f30
Teyit <ajimamatovteyit@gmai.com>
2023-08-18 16:13:11 +0600

    Add need url

--- main.swift
+++ main.swift
@@ -5,3 +5,4 @@ print("Add code from git clone")
 print("Now add from original file")
 for i in 1...100 {
   print(i)
+    // git remote set-url origin https://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@github.com/Teyitbek/div.git
